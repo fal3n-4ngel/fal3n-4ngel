526baf55ce144ea3170f76fbcb7b1d0f01d5872a
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-11-17 16:10:19 +0530

    update : speed up animations

--- app/components/FadeUp.tsx
+++ app/components/FadeUp.tsx
@@ -11,11 +11,11 @@ const FadeUp: React.FC<FadeUpProps> = ({ children }) => {
   return (
     <motion.div
       ref={ref}
-      initial={{ opacity: 0, y: 40 }}
+      initial={{ opacity: 0, y: 30 }}
       animate={inView ? { opacity: 1, y: 0 } : {}}
       transition={{
-        duration: 0.7,
-        delay: 0.2,
+        duration: 0.5,
+        delay: 0.1,
       }}
       
     >
