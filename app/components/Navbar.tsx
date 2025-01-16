33295512861ad25666fbf76bc0456eb481cbd200
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2025-01-16 19:40:37 +0530

    update (resume) : updated resume links in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -37,7 +37,7 @@ function Navbar() {
           linkedin
         </a>
         <a
-          href="/Resume Adithya Krishnan Nov.pdf"
+          href="/Resume Adithya Krishnan.pdf"
           className="resumeLogo font-poppins text-[2.5rem] text-black transition-all duration-300 hover:opacity-25 dark:text-white md:text-[1vw]"
         >
           resume
