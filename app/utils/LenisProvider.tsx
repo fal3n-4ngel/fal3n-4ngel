52a325ccef3bca8d3f4250135252f5e97ae6a791
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2025-06-06 18:51:12 +0530

    chore: update deprecated dependencies

--- app/utils/LenisProvider.tsx
+++ app/utils/LenisProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function LenisProvider({
   children,
