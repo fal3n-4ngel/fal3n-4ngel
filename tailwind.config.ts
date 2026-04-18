ffca22fcafa475c0400a73110531d03afb80eb8d
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2026-04-18 16:48:36 +0530

    update : fix css error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: "class",
   content: [
-    "./pages*.{js,ts,jsx,tsx,mdx}",
-    "./components*.{js,ts,jsx,tsx,mdx}",
-    "./sections*.{js,ts,jsx,tsx,mdx}",
-    "./app*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
