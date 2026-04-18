70372fbe53ce197a148fce6eec1508fc2414d62c
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2026-04-18 15:14:47 +0530

    fix : fix nav bar issues

--- app/components/sections/Navbar.tsx
+++ app/components/sections/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
       initial={{ y: -100 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
-      className="interactable fixed top-0 z-50 w-full px-12 py-10 mix-blend-difference md:pt-20"
+      className="interactable fixed top-0 z-50 w-full px-12 py-10 mix-blend-difference md:py-5 md:pt-[10vh]"
     >
       <div className="mx-auto flex w-full max-w-6xl justify-between">
         <div className="font-space font-mono text-4xl font-light tracking-wider text-white/90">
